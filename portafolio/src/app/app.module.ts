f2d72d8715eb2e2d44b9cae67bb87d0c0e3c6d51
Darwin Guzmán <dga_guz@hotmail.es>
2018-08-25 12:27:53 -0500

    Configuración de rutas implementada

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { PortafolioComponent } from './pages/portafolio/portafolio.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ItemComponent } from './pages/item/item.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {PortafolioComponent} from './pages/portafolio/portafolio.component';
+import {AboutComponent} from './pages/about/about.component';
+import {ItemComponent} from './pages/item/item.component';
+import {AppRouting} from "./app.routing";
 
 @NgModule({
   declarations: [
@@ -18,9 +19,11 @@ import { ItemComponent } from './pages/item/item.component';
     ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
