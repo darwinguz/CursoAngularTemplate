ab86b9b1f11eb4d2a3a69d306826e5932fb848d3
Darwin Guzmán <dga_guz@hotmail.es>
2018-08-26 12:18:27 -0500

    Implementacion de lectura en la base de datos de Firebase en la pagina About

--- portafolio/src/app/services/info-pagina.service.ts
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -8,19 +8,34 @@ import {InfoPaginaInterface} from "../interfaces/info-pagina.interface";
 export class InfoPaginaService {
   info: InfoPaginaInterface = {};
   cargada: boolean = false;
+  equipo: any[] = [];
 
   constructor(private _http: HttpClient) {
     console.log('InfoPaginaService se inicializó...');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarEquipo() {
+    this._http.get('https://angulartemplate-c0ba8.firebaseio.com/equipo.json').subscribe(
+      (value: any[]) => {
+        this.equipo = value;
+        console.log(this.equipo);
+      }
+    );
+  }
+
+  private cargarInfo() {
     //lectura de archivo JSON
     this._http.get('assets/data/data-pagina.json')
       .subscribe(
         (value: InfoPaginaInterface) => {
-          console.log(value);
+          // console.log(value);
           this.info = value;
           this.cargada = true;
           //solucion elegante para obtener el atributo o propiedad y no hacer a value any (en el caso de no tener interface)
           //console.log(value['twitter']);
         }
-      )
+      );
   }
 }
